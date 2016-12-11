f8bf14819602c4a615be0471eb8111f729bcb424
CJ Sampson <cjsampson41@gmail.com>
2016-12-11 16:42:17 -0700

    pictures added for the grocery items

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                Grocery List App
+                <img src="../images/bread.jpeg" />
+            </div>
+        );
+    }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-// Set up your application entry point here...
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+import App from './components/App';
+
+
+ReactDOM.render(<App />, document.getElementById('app'));
