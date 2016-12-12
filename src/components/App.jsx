2893502a835b9a8e5a9561f8ab58b0039db93eee
CJ Sampson <cjsampson41@gmail.com>
2016-12-11 17:17:01 -0700

    map through all grocery pictures with

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 
+import groceryData from '../data/groceryData';
+
+
 class App extends Component {
+    constructor() {
+        super(); 
+        this.state = {
+            items: groceryData
+        }
+    }
     render() {
         return (
             <div>
-                Grocery List App
-                <img src="../images/bread.jpeg" />
+                <ul>
+                    {groceryData.map(item =>
+                        <li key={item.id}>
+                            <img src={item.image} />
+                            {item.item}
+                        </li>                        
+                    )}
+                </ul>
             </div>
         );
     }
---
+++ src/data/groceryData.js
@@ -0,0 +1,112 @@
+const groceryData = [
+    {
+        'id': 1,
+        'item': 'bread',
+        'price': 3.29,
+        'image': '../images/bread.jpeg'
+    },
+    {
+        'id': 2,
+        'item': 'cheese',
+        'price': 3.78,
+        'image': '../images/cheese.jpeg'
+    },
+    {
+        'id': 3,
+        'item': 'kefir',
+        'price': 2.39,
+        'image': '../images/kefir.png'
+    },
+    {
+        'id': 4,
+        'item': 'mustard',
+        'price': 1.59,
+        'image': '../images/mustard.jpeg'
+    },
+    {
+        'id': 5,
+        'item': 'olive oil',
+        'price': 2.99,
+        'image': '../images/olive-oil.jpeg'
+    },
+    {
+        'id': 6,
+        'item': 'oranges',
+        'price': 3.00,
+        'image': '../images/orange.jpeg'
+    },
+    {
+        'id': 7,
+        'item': 'paper towels',
+        'price': 3.25,
+        'image': '../images/paper-towl.jpeg'
+    },
+    {
+        'id': 8,
+        'item': 'pasta',
+        'price': 2.10,
+        'image': '../images/pasta.jpeg'
+    },
+    {
+        'id': 9,
+        'item': 'peanut-butter',
+        'price': 4.00,
+        'image': '../images/peanut-butter.jpeg'
+    },
+    {
+        'id': 10,
+        'item': 'pepper',
+        'price': 1.25,
+        'image': '../images/pepper.jpeg'
+    },
+    {
+        'id': 11,
+        'item': 'rice',
+        'price': 5.19,
+        'image': '../images/rice.jpeg'
+    },
+    {
+        'id': 12,
+        'item': 'salmon',
+        'price': 7.89,
+        'image': '../images/salmon.jpeg'
+    },
+    {
+        'id': 13,
+        'item': 'salt',
+        'price': 1.25,
+        'image': '../images/salt.jpeg'
+    },
+    {
+        'id': 14,
+        'item': 'soy sauce',
+        'price': 2.26,
+        'image': '../images/soy-sauce.jpeg'
+    },
+    {
+        'id': 15,
+        'item': 'spinach',
+        'price': 4.39,
+        'image': '../images/spinach.jpeg'
+    },
+    {
+        'id': 16,
+        'item': 'sriracha',
+        'price': 3.45,
+        'image': '../images/sriracha.jpeg'
+    },
+    {
+        'id': 17,
+        'item': 'tomatos',
+        'price': 2.88,
+        'image': '../images/tomato.jpeg'
+    },
+    {
+        'id': 18,
+        'item': 'water bottle',
+        'price': 2.68,
+        'image': '../images/water.jpeg'
+    },
+];
+
+export default groceryData;
